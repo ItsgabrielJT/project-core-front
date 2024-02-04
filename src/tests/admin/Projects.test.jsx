64c59322e51f7a8f4d9ddd37c574926f832cf58e
ItsgabrielJT <gabrielasimbaya82@gmail.com>
2024-02-04 17:59:38 -0500

    admin tresting

---
+++ src/tests/admin/Projects.test.jsx
@@ -0,0 +1,14 @@
+import { describe, expect, test } from "vitest";
+
+describe("Admin Projects component", () => {
+  test("Renderizar card de proyectos", () => {
+    expect(1 + 1).toBe(2);
+  });
+  test("Renderizar modal de confirmacion de eliminacion", () => {
+    expect(1 + 1).toBe(2);
+  });
+  test("Funcionalidad boton de eliminacion", () => {
+    expect(1 + 1).toBe(2);
+  });
+  
+});
--- src/tests/admin/User.test.jsx
+++ src/tests/admin/User.test.jsx
@@ -1,16 +1,16 @@
 import { describe, expect, test } from "vitest";
 
-describe("User component", () => {
+describe("Admin users component", () => {
   test("Renderizar pantalla de detalle de perfil", () => {
     expect(1 + 1).toBe(2);
   });
-  test("Renderizar pantalla de detalle de proyecto", () => {
+  test("Renderizar pantalla de listado de usuarios", () => {
     expect(1 + 1).toBe(2);
   });
   test("Funcionalidad del boton de edicion", () => {
     expect(1 + 1).toBe(2);
   });
-  test("Renderizado del componente de estadisticas", () => {
+  test("Funcionalidad del boton de eliminacion de usuario", () => {
     expect(1 + 1).toBe(2);
   });
 });
